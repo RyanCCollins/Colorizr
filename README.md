52d29b75b40c7709cfec7a98df0178db430da1fe
Ryan Collins <rccproductions@mac.com>
2016-03-11 00:45:48 -0500

    Feat: update with background color

--- README.md
+++ README.md
@@ -5,19 +5,32 @@ A ruby gem that changes the output of text to the terminal.
 Colorizr
 
 ## Getting Started
-
+You will want to download this repo, or the colorizr-0.0.2.gem file and run
 ```
-gem 'colorizr'
+sudo gem install colorizr-0.0.2
 ```
 
-### Installing
+To use it, just `require 'colorizr'`
 
+## Usage
+You can use this handy gem to colorize the output of your test to the terminal.  First, type `require 'colorizr'` in the top of your file.  This expands the String class, allowing you to call a number of different methods on any string in order to colorize it.
 
-## Deployment
+To get a list of the colors available, try something like
+```
+String.color
+```
+Which will list out the available colors.  Also, to get a sample, you can run
+```
+String.sample_colors
+```
 
+Finally, you can also see the available background colors.  To do so, call
+```
+String.background_colors
+```
 
 ## Built With
-
+Ruby
 
 ## Authors
 
--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,6 +1,24 @@
 class String
-  @@colors = {red: , green:, yellow:, blue: , pink: , light_blue: , white: , light_grey: , black:, }
+  # Colors hash holds all of the standard commandline color values
+    # The pink is actually light_magenta
+  @@colors = { red: 31, green: 32, yellow: 33, blue: 34,
+               magenta: 35, pink: 95, light_blue: 94, white: 97,
+               light_grey: 37, black: 30, cyan: 36, dark_gray: 90,
+                light_red: 91, light_green: 92 }
 
+  @@background_colors = { black: 40, red: 41, green: 42, yellow: 43,
+                          blue: 44, magenta: 45, cyan: 46, light_grey: 47,
+                          dark_gray: 100, light_red: 101, light_green: 102,
+                          light_yellow: 103, light_blue: 104, light_magenta: 105,
+                          pink: 105, light_cyan: 106, white: 107 }
+
+  # If a method is called that does not exist,
+    # Then call the method missing.
+  def method_missing sym
+    puts "#{sym} does nothing.  I said nothing!"
+  end
+
+  # Create the color methods for every value in the @@colors array
   def self.create_colors
     @@colors.each do |color, value|
       self.send(:define_method, color) do
@@ -9,14 +27,40 @@ class String
     end
   end
 
+  # Create the background colors, which are accessible
+    # by calling the color name followed by _background
+  def self.create_backgrounds
+    @@background_colors.each do |color, value|
+      name = (color.to_s + '_background').to_sym
+      self.send(:define_method, name) do
+        "\e[#{value}m" + self + "\e[0m"
+      end
+    end
+  end
+
+  # Return the colors in a usable array by calling Colorizr.colors
   def self.colors
     @@colors.each do |color, value|
       colors << color
     end
+    colors
   end
 
+  def self.background_colors
+    @@background_colors.each do |color, value|
+      background_colors << color
+    end
+    background_colors
+  end
+
+  # Give a sample of all of the available colors.  Takes
+    # an option for showing background colors
   def self.sample_colors
     @@colors.each do |color, value|
+      puts "This is " + "#{color}".send(color)
+    end
+    # Sample background colors
+    @@background_colors.each do |color, value|
       puts "This is " + "#{color}".send(color)
     end
   end
@@ -24,3 +68,4 @@ end
 
 # Create the colors for the String class
 String.create_colors
+String.create_backgrounds
